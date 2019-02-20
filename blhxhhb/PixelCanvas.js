45b50ad2fbfa4a6ca112984ada52095d5bdab7e3
Lo-Dyih-Yuaan <42846989+Lo-Dyih-Yuaan@users.noreply.github.com>
2019-02-20 20:06:20 +0800

    Create PixelCanvas.js

---
+++ blhxhhb/PixelCanvas.js
@@ -0,0 +1,70 @@
+function PixelCanvas(canvas, width, height, pixelColors, defaultColor, defaultPattern = defaultColor,
+                     borderColor = 'gray', pixelSize = 15, borderSize = 1) {
+	this.canvas = canvas;
+	this.width = width;
+	this.height = height;
+	this.pixelColors = Object.assign({}, pixelColors);
+	this.defaultColor = defaultColor;
+	this.data = JSON.parse(JSON.stringify(new Array(this.height).fill([])));
+	this.borderColor = borderColor;
+	this.pixelSize = pixelSize;
+	this.borderSize = borderSize;
+	this.initalize();
+	this.paint(defaultPattern);
+	return this;
+}
+PixelCanvas.prototype = {
+	constructor: PixelCanvas,
+	initalize: function () {
+		let ctx = this.canvas.getContext("2d");
+		let elementSize = this.pixelSize + this.borderSize;
+		this.canvas.width = this.width*elementSize+this.borderSize;
+		this.canvas.height = this.height*elementSize+this.borderSize;
+		ctx.fillStyle = this.borderColor;
+		ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+		this.fillColor = this.defaultColor;
+	},
+	daub: function (i, j) {
+		let ctx = this.canvas.getContext("2d");
+		let elementSize = this.pixelSize + this.borderSize;
+		ctx.fillRect(j*elementSize+this.borderSize,
+		             i*elementSize+this.borderSize,
+		             this.pixelSize, this.pixelSize);
+		this.data[i][j] = this.fillColorName;
+	},
+	paint: function (patternStr) {
+		let temp = this.fillColor;
+		let pattern = patternStr.split(/(?=[A-Z])/);
+		if (pattern.length == 1) {
+			this.fillColor = pattern[0];
+			for (let i = 0; i < this.height; ++i)
+				for (let j = 0; j < this.width; ++j)
+					this.daub(i, j);
+		} else if (pattern.length == this.width*this.height) {
+			for (let i = 0; i < this.height; ++i)
+				for (let j = 0; j < this.width; ++j) {
+					this.fillColor = pattern[i*this.width+j];
+					this.daub(i, j);
+				}
+		} else {
+			for (let i = 0; i < this.height; ++i)
+				for (let j = 0; j < this.width; ++j) {
+					this.fillColor = pattern[(i*this.width+j)%pattern.length];
+					this.daub(i, j);
+				}
+		}
+		this.fillColor = temp;
+		console.log(pattern.length, this.width*this.height);
+	},
+	set fillColor(fillColor) {
+		this.fillColorName = fillColor;
+		let ctx = this.canvas.getContext("2d");
+		ctx.fillStyle = this.pixelColors[fillColor];
+	},
+	get fillColor() {
+		return this.fillColorName;
+	},
+	get pattern() {
+		return this.data.map(arr => arr.join("")).join("");
+	}
+};
